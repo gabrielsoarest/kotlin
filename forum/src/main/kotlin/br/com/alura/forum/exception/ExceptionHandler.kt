96fe80078bda51f1dc6e1f6b7c6bed206d79473f
Gabriel Soares <gabriel.pinto@azulseguros.com.br>
2021-09-26 20:19:16 -0300

    Inclusao de Exceptions

--- forum/src/main/kotlin/br/com/alura/forum/exception/ExceptionHandler.kt
+++ forum/src/main/kotlin/br/com/alura/forum/exception/ExceptionHandler.kt
@@ -2,6 +2,7 @@ package br.com.alura.forum.exception
 
 import br.com.alura.forum.dto.ErrorView
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -21,9 +22,23 @@ class ExceptionHandler {
         )
     }
 
-    @ExceptionHandler(Exception::class)
+    @ExceptionHandler(MethodArgumentNotValidException::class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun handleNotFound(exception: Exception, request: HttpServletRequest): ErrorView {
+    fun handleServerError(exception: MethodArgumentNotValidException, request: HttpServletRequest)
+    : ErrorView {
+
+     val errorMessage = HashMap<String, String?> ()
+        exception.bindingResult.fieldErrors.forEach { e-> errorMessage.put(e.field,e.defaultMessage) }
+
+        return ErrorView(
+                status = HttpStatus.BAD_REQUEST.value(),
+                error = HttpStatus.BAD_REQUEST.name,
+                message = errorMessage.toString(),
+                path = request.servletPath
+            )
+    }
+
+    fun handleValidationError(exception: Exception, request: HttpServletRequest): ErrorView {
         return ErrorView(
             status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
             error = HttpStatus.INTERNAL_SERVER_ERROR.name,
